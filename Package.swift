086c25243bebd7c1e39dd9df40f554c031da3335
Juraj Antas <juraj.antas@gmail.com>
2023-07-06 14:45:54 +0200

    updating package to 4.1.17

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "SygicUbiKit",
-            url: "",
+            url: "https://github.com/jurajantas/UbiTestRepo/releases/download/4.1.17/SygicUbiKit.xcframework-4.1.17.zip",
             checksum: "9654aab914f8a62ac8f26373b99c3c6e04e86d97b231e1530d4390869c590ac5")
     ]
 )
